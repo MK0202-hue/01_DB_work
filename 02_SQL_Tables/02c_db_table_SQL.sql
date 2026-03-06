6a811ac75d43e78ac38c25836e62e3ab508ae0e0
M.Kapunkt <m.kaletta0202@gmail.com>
2026-03-06 08:46:46 +0100

    update

---
+++ 02_SQL_Tables/02c_db_table_SQL.sql
@@ -0,0 +1,36 @@
+\! cls
+/* ------  Strukturen ----- */
+
+/* 	 
+	Doppelte DS werden nicht mehr zugelassen
+    UNIQUE
+*/
+
+/* Tabelle test löschen, falls vorhanden*/
+DROP TABLE IF EXISTS boo.test; -- db.table
+
+/* Tab. test anlegen, falls noch nicht vorhanden*/
+CREATE TABLE IF NOT EXISTS boo.test (
+    name VARCHAR(20) NOT NULL DEFAULT "TBA" UNIQUE,
+    age INT NOT NULL DEFAULT 0
+);
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE boo.test;
+
+/* ----- Daten ------- */
+INSERT INTO boo.test(name,age) VALUES("Max",35);
+INSERT INTO boo.test(age,name) VALUES(29,"Maxine");
+INSERT INTO boo.test VALUES();
+
+
+-- Doppelte Datensätze werden NICHT mehr zugelassen !
+INSERT INTO test(name,age) VALUES("Max Jackson",35);
+
+
+/* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM boo.test;
+
+
+
+
---
+++ 03_user_DB/03b_db_userDB_SQL.sql
@@ -0,0 +1,20 @@
+\! cls
+/* USER TAB. Version 2 */
+
+/* Table users */
+DROP TABLE IF EXISTS boo.users;
+
+-- Create ....
+-- userPwd / varchar 40 / not null
+
+/* Struktur */
+DESCRIBE boo.users;
+
+/* Daten */
+
+--pwd ergänzen
+
+/* Inhalte : Ergebnistabelle */
+SELECT * FROM boo.users;
+
+
