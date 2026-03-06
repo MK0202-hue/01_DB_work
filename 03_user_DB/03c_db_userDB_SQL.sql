c3c4ebad623b750701c3603a3ea27e64d19c9f42
M.Kapunkt <m.kaletta0202@gmail.com>
2026-03-06 11:02:35 +0100

    update 03_02 passwort verschleierung

--- 03_user_DB/03c_db_userDB_SQL.sql
+++ 03_user_DB/03c_db_userDB_SQL.sql
@@ -1,5 +1,5 @@
 \! cls
-/* USER TAB. Version 2 */
+/* USER TAB. Version 3 */
 
 /* Table users */
 DROP TABLE IF EXISTS boo.users;
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS boo.users
 (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     userName VARCHAR(20) NOT NULL UNIQUE,
-    userPwd VARCHAR(40) NOT NULL,
+    userPwd VARCHAR(64) NOT NULL,
     familyName VARCHAR(20) NOT NULL,
     firstName VARCHAR(20) NOT NULL
 );
@@ -18,14 +18,18 @@ DESCRIBE boo.users;
 
 /* Daten */
 INSERT INTO boo.users (userName, familyName, firstName, userPwd) 
-VALUES ('johndoe123', 'Doe', 'John', '1234');
+VALUES ('johndoe123', 'Doe', 'John', SHA2('1234', 256));
 
 INSERT INTO boo.users (userName, familyName, firstName, userPwd) 
-VALUES ('emilybrown22', 'Brown', 'Emily', '7xU#');
+VALUES ('emilybrown22', 'Brown', 'Emily', SHA1('7xU#'));
 
 INSERT INTO boo.users (userName, familyName, firstName, userPwd) 
-VALUES ('michaelsmith7', 'Smith', 'Michael', 'user1234');
+VALUES ('michaelsmith7', 'Smith', 'Michael', SHA1('user1234'));
 
 
 /* Inhalte : Ergebnistabelle */
 SELECT * FROM boo.users;
+
+
+
+
